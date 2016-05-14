565537a588326ad1cc12871267274026affd1d3e
Francisco de Maussion <franciscodemaussion@gmail.com>
2016-05-14 17:37:13 -0300

    Login con google funcional

--- www/js/app.js
+++ www/js/app.js
@@ -3,99 +3,49 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers','ngCordova','ionic-toast', 'ionic-timepicker', 'ionic-datepicker'])
+angular.module('starter', ['ionic', 'controllers', 'services'])
 
-  .run(function($ionicPlatform) {
+.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
-
     }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
+    if(window.StatusBar) {
       StatusBar.styleDefault();
     }
   });
 })
-  .config(function (ionicDatePickerProvider) {
-    var datePickerObj = {
-      inputDate: new Date(),
-      setLabel: 'Aceptar',
-      todayLabel: 'Hoy',
-      closeLabel: 'Cerrar',
-      mondayFirst: true,
-      weeksList: ["L", "M", "M", "J", "V", "S", "D"],
-      monthsList: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"],
-      templateType: 'popup',
-      showTodayButton: false,
-      dateFormat: 'dd MMMM yyyy',
-      closeOnSelect: false,
-      disableWeekdays: [0],
-    };
-    ionicDatePickerProvider.configDatePicker(datePickerObj);
-  })
-  .config(function (ionicTimePickerProvider) {
-      var timePickerObj = {
-        inputTime: (((new Date()).getHours() * 60 * 60) + ((new Date()).getMinutes() * 60)),
-        format: 24,
-        step: 1,
-        setLabel: 'Aceptar',
-        closeLabel: 'Cerrar'
-      };
-      ionicTimePickerProvider.configTimePicker(timePickerObj);
-    })
-  .config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
-  $ionicConfigProvider.tabs.position('bottom');
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
+.config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
 
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: '/tab',
+  .state('welcome', {
+    url: '/welcome',
+    templateUrl: "views/welcome.html",
+    controller: 'WelcomeCtrl'
+  })
+
+  .state('app', {
+    url: "/app",
     abstract: true,
-    templateUrl: 'templates/tabs.html'
+    templateUrl: "views/sidemenu.html",
+    controller: 'AppCtrl'
   })
 
-    .state('tab.noticias', {
-    url: '/noticias',
+  .state('app.home', {
+    url: "/home",
     views: {
-      'tab-noticias': {
-        templateUrl: 'templates/tab-noticias.html',
-        controller: 'noticiasCtrl'
+      'menuContent': {
+        templateUrl: "views/home.html",
+        controller: 'HomeCtrl'
       }
     }
   })
 
-    .state('tab.crear', {
-    url: '/crear',
-    views: {
-      'tab-crear': {
-        templateUrl: 'templates/tab-crear.html',
-        controller: 'CreateCtrl'
-      }
-    }
-  })
-
-    .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
-  });
+  ;
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/noticias');
-
-});
+  $urlRouterProvider.otherwise('/welcome');
+})
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,145 +1,75 @@
-app = angular.module('starter.controllers', [])
+angular.module('controllers', [])
 
-app.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+.controller('WelcomeCtrl', function($scope, $state, UserService, $ionicLoading) {
 
-app.controller('CreateCtrl', function($scope,ionicTimePicker, ionicDatePicker, ionicToast) {
-  $scope.fyh="Fecha y Hora";
-  var t;
-  var d;
-  function addZero(i) {
-    if (i < 10) {
-      i = "0" + i;
-    }
-    return i;
-  }
-  var tp = {
-    callback: function (val) {      //Mandatory
-      if (typeof (val) === 'undefined') {
-        console.log('Time not selected');
-      } else {
-        var selectedTime = new Date(val * 1000);
-        console.log('Selected epoch is : ', val, 'and the time is ', selectedTime.getUTCHours(), 'H :', selectedTime.getUTCMinutes(), 'M');
-        t =  addZero(selectedTime.getUTCHours())+":"+ addZero(selectedTime.getUTCMinutes());
-        $scope.fyh=d+"-"+t;
-        ionicToast.show($scope.fyh, 'bottom', false, 2500);
-      }
-    },
-  };
-  var dt = {
-    callback: function (val) {  //Mandatory
-      console.log('Return value from the datepicker popup is : ' + val, new Date(val));
-      d = new Date(val);
-      m = d.getMonth()+1
-      d = d.getDate()+"-"+m+"-"+d.getFullYear();
-      ionicTimePicker.openTimePicker(tp);
-    },
-  };
-
-  $scope.date = function(){
-    ionicDatePicker.openDatePicker(dt);
-  }
-  $scope.sendNew= function() {
-    ionicToast.show($scope.eTitulo, 'bottom', false, 2500);
-    if(window.plugins && window.plugins.emailComposer) {
-      window.plugins.emailComposer.showEmailComposerWithCallback(function(result) {
-        console.log("Response -> " + result);
-      },
-     $scope.eTitulo, // Subject
-     $scope.eContent+" event-data="+$scope.fyh,                      // Body
-     [""],    // To
-     null,                    // CC
-     null,                    // BCC
-     false,                   // isHTML
-     null,                    // Attachments
-     null);                   // Attachment Data
-    }
-  };
-});
-
-app.controller("noticiasCtrl", function($http, $scope, ionicToast, $ionicPopup, $cordovaCalendar) {
-
-  $scope.init = function() {
-    $http.get("http://ajax.googleapis.com/ajax/services/feed/load", { params: { "v": "1.0", "q": "https://feeds.feedburner.com/Appteneo" } })
-      .success(function(data) {
-      $scope.rssTitle = data.responseData.feed.title;
-      $scope.rssUrl = data.responseData.feed.feedUrl;
-      $scope.rssSiteUrl = data.responseData.feed.link;
-      $scope.entries = data.responseData.feed.entries;
-      window.localStorage["entries"] = JSON.stringify(data.responseData.feed.entries);
-    })
-      .error(function(data) {
-      console.log("ERROR: " + data);
-      if(window.localStorage["entries"] !== undefined) {
-        $scope.entries = JSON.parse(window.localStorage["entries"]);
-        ionicToast.show('No es posible cargar', 'bottom', false, 2500);
-      }
-    });
-  }
-
-  $scope.refresh=function(){
-    $http.get("http://ajax.googleapis.com/ajax/services/feed/load", { params: { "v": "1.0", "q": "https://feeds.feedburner.com/Appteneo" } })
-      .success(function(data) {
-      $scope.rssTitle = data.responseData.feed.title;
-      $scope.rssUrl = data.responseData.feed.feedUrl;
-      $scope.rssSiteUrl = data.responseData.feed.link;
-      $scope.entries = data.responseData.feed.entries;
-      window.localStorage["entries"] = JSON.stringify(data.responseData.feed.entries);
-    })
-      .error(function(data) {
-      console.log("ERROR: " + data);
-      if(window.localStorage["entries"] !== undefined) {
-        $scope.entries = JSON.parse(window.localStorage["entries"]);
-        ionicToast.show('No es posible cargar', 'bottom', false, 2500);
-      }
-    }).finally(function() {
-      // Stop the ion-refresher from spinning
-      $scope.$broadcast('scroll.refreshComplete');
-    });
-  }
-
-  $scope.cutEvent = function(string, nb, ct) {
-    array = string.split(ct);
-    return array[nb];
-  }
-
-  $scope.showConfirm = function(titulo,lugar,notas,año,mes,dia,hora,min) {
-    $scope.createEvent;
-    var confirmPopup = $ionicPopup.confirm({
-      title: 'Recordarme '+ titulo,
-      template: '¿Desea que le recordemos del evento?',
-      cancelText: 'Cancelar',
-      okText: 'Aceptar',
+  //This method is executed when the user press the "Login with Google" button
+  $scope.googleSignIn = function() {
+    $ionicLoading.show({
+      template: 'Logging in...'
     });
 
-    confirmPopup.then(function(res) {
-      if(res) {
-        console.log('You are sure');
-        $scope.createEvent(titulo,lugar,notas,año,mes,dia,hora,min);
-      } else {
-        console.log('You are not sure');
-      }
-    });
+    window.plugins.googleplus.login(
+        {},
+        function (user_data) {
+          console.log(user_data);
+
+          //for the purpose of this example I will store user data on local storage
+          UserService.setUser({
+            userID: user_data.userId,
+            name: user_data.displayName,
+            email: user_data.email,
+            picture: user_data.imageUrl,
+            accessToken: user_data.accessToken,
+            idToken: user_data.idToken
+          });
+
+          $ionicLoading.hide();
+          $state.go('app.home');
+        },
+        function (msg) {
+          $ionicLoading.hide();
+          console.log(msg);
+        }
+    );
   };
-
-  $scope.createEvent = function(titulo,lugar,notas,año,mes,dia,hora,min) {
-    $cordovaCalendar.createEvent({
-      title: titulo,
-      location: lugar,
-      notes: notas,
-      startDate: new Date('20'+año, mes-1, dia, hora, min, 0, 0, 0),//año, mes(desde 0),dia,hora,min
-      endDate: new Date('20'+año, mes-1, dia, hora, min, 0, 0, 0)
-    }).then(function (result) {
-      console.log("Event created successfully");
-      ionicToast.show('Evento creado', 'bottom', false, 2500);
-    }, function (err) {
-      console.error("There was an error: " + err);
-      ionicToast.show('No se pudo crear el evento', 'bottom', false, 2500);
-    });
-  }
+})
 
 
-});
+.controller('AppCtrl', function($scope){
+
+})
+
+.controller('HomeCtrl', function($scope, UserService, $ionicActionSheet, $state, $ionicLoading){
+
+	$scope.user = UserService.getUser();
+
+	$scope.showLogOutMenu = function() {
+		var hideSheet = $ionicActionSheet.show({
+			destructiveText: 'Logout',
+			titleText: '¿Seguro que queres salir?',
+			cancelText: 'Cancelar',
+			cancel: function() {},
+			buttonClicked: function(index) {
+				return true;
+			},
+			destructiveButtonClicked: function(){
+				$ionicLoading.show({
+					template: 'Logging out...'
+				});
+				//google logout
+				window.plugins.googleplus.logout(
+					function (msg) {
+						console.log(msg);
+						$ionicLoading.hide();
+						$state.go('welcome');
+					},
+					function(fail){
+						console.log(fail);
+					}
+				);
+			}
+		});
+	};
+})
+
+;
